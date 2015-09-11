06d1721e29ea2118077493fa9984b43c755c17d6
sleepbear <sleepbear669@gmail.com>
2015-09-11 21:39:51 +0900

    apply angular js add reportController

---
+++ js/controller/reportController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by sleepbear on 15. 9. 11..
+ */
+disasterApp.controller('reportController', function($scope){
+    $scope.bindingTest = 'hello world';
+})
---
+++ js/disasterApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by sleepbear on 15. 9. 11..
+ */
+var disasterApp = angular.module('disasterApp', []);
