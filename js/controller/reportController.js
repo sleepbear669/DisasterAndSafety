5fa305f9b0c5fabf5ab6394c5a55791c05c83947
sleepbear <sleepbear669@gmail.com>
2015-09-12 09:38:25 +0900

    can view only localmedia

--- js/controller/reportController.js
+++ js/controller/reportController.js
@@ -2,6 +2,20 @@
  * Created by sleepbear on 15. 9. 11..
  */
 disasterApp.controller('reportController', function($scope, socket){
+
+
+    var errorCallback = function(e) {
+        console.log('Reeeejected!', e);
+    };
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    navigator.getUserMedia({video: true, audio: false}, function (localMediaStream) {
+        var localCamera = document.getElementById("local");
+        localCamera.src = window.URL.createObjectURL(localMediaStream);
+    }, errorCallback);
+
+
     socket.on("message", function(data){
         console.log(data);
     })
