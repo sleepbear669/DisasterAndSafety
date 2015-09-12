c1392cd24020909e25b8e4a3e4a89f45e0ba1416
sleepbear <sleepbear669@gmail.com>
2015-09-12 10:29:07 +0900

    can view remote stream media

--- js/controller/reportController.js
+++ js/controller/reportController.js
@@ -3,22 +3,79 @@
  */
 disasterApp.controller('reportController', function($scope, socket){
 
-
     var errorCallback = function(e) {
         console.log('Reeeejected!', e);
     };
+    var localDesc;
+    var remoteDecs;
+    var localVideo = document.getElementById('local');
+    var remoteVideo = document.getElementById('remote');
+    var localVideoStream;
     navigator.getUserMedia = navigator.getUserMedia ||
         navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    var configuration = {
+        "iceServers": [
+            {
+                "url": "stun:mmt-stun.verkstad.net"
+            },
+            {
+                "url": "turn:mmt-turn.verkstad.net",
+                "username": "webrtc",
+                "credential": "secret"
+            }
+        ]
+    };
 
+    var pc = new webkitRTCPeerConnection(configuration);
     navigator.getUserMedia({video: true, audio: false}, function (localMediaStream) {
-        var localCamera = document.getElementById("local");
-        localCamera.src = window.URL.createObjectURL(localMediaStream);
+        console.log(window.URL.createObjectURL(localMediaStream));
+        localVideo.src = window.URL.createObjectURL(localMediaStream);
+
+        localVideoStream = localMediaStream;
+        pc.addStream(localVideoStream);
+        init();
     }, errorCallback);
 
+    var init = function () {
+        pc.onaddstream = function (obj) {
+            remoteVideo.src = window.URL.createObjectURL(obj.stream);
+            remoteVideo.play();
+        };
+        pc.onicecandidate = function (event) {
+            if (!event || !event.candidate) return;
+            socket.emit("icecandidate", event.candidate);
+        };
+    }
+    $scope.start = function () {
+        console.log("strat");
+        pc.createOffer(function (desc) {
+            pc.setLocalDescription(desc, function () {
+                localDesc = desc;
+                socket.emit("offer", desc);
+            });
+        });
+    };
+    socket.on("message", function (message) {
+        if(message.type == "offer") {
+            remoteDecs = message;
+        }
 
-    socket.on("message", function(data){
-        console.log(data);
-    })
-    console.log("Dtad");
-    $scope.bindingTest = 'hello world';
+    });
+    socket.on("offer", function(offer) {
+        remoteDecs = offer;
+        pc.setRemoteDescription(new RTCSessionDescription(offer));
+        pc.createAnswer(function (answer) {
+            pc.setLocalDescription(answer);
+            socket.emit("answer", answer);
+        });
+    });
+    socket.on("addCandidate", function (candidate) {
+        pc.addIceCandidate(new RTCIceCandidate(candidate));
+    });
+    socket.on("complete", function (answer) {
+        pc.setRemoteDescription(new RTCSessionDescription(answer));
+    });
+    socket.on("first", function(socket){
+        console.log(socket);
+    });
 })
